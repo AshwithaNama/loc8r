d3c4c6380f9c64ee0ddd6a3b53e889256f7944ac
22eg107c33@anurag.edu.in <22eg107c33@anurag.edu.in>
2024-09-10 13:55:22 -0700

    firstcommit

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -2,3 +2,7 @@ module.exports. index=function(req ,res)
 {
     res.render('index', { title: 'Express' });
 };
+module.exports. login=function(req ,res)
+{
+    res.render('login', { title: 'Welcome' });
+};
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,18 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var ctrlMain = require('../controllers/main');
+var ctrlothers= require('../controllers/others');
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', ctrlMain.index);
+
 router.get('/location', function(req, res, next) {
   res.render('index', { title: 'Location info' });
 });
 router.get('/location/review', function(req, res, next) {
   res.render('index', { title: 'Review' });
 });
-router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'about' });
-});
+router.get('/about',ctrlothers.about);
 
+router.get('/login',ctrlMain.login );
+
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'register' });
+});
+//exports command//
 module.exports = router;
+
+
+
+
